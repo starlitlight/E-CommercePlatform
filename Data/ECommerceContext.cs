3d2d54be01e31b3b4c54febecf27788f58c8925e
Fangming Lan <starlitlight@users.noreply.github.com>
2024-06-14 14:15:06 +0200

    Construct ECommerceContext.cs

---
+++ Data/ECommerceContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+
+public class MyApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+        if(string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("DB connection string is not set in environment variables.");
+        }
+        
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
