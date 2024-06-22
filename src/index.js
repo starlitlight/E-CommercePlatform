77a4397db30e2421ca146a0cd52be3f8afe48da9
Fangming Lan <starlitlight@users.noreply.github.com>
2024-06-22 12:28:12 +0200

    update with new feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createContext, useContext, useState } from 'react';
+
 const apiUrl = process.env.REACT_APP_API_URL;
-ReactDOM.render(<App apiUrl={apiUrl} />, document.getElementById('root'));
+
+const GlobalContext = createContext();
+
+export const GlobalProvider = ({ children }) => {
+  const [globalState, setGlobalState] = useState({
+    isAuthenticated: false,
+    cartProps: [],
+  });
+
+  const updateGlobalState = (newState) => {
+    setGlobalState((prevState) => ({ ...prevState, ...newState }));
+  };
+
+  return (
+    <GlobalContext.Provider value={{ globalState, updateGlobalState }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+ReactDOM.render(
+  <Router>
+    <GlobalProvider>
+      <App apiUrl={apiUrl} />
+    </GlobalProvider>
+  </Router>,
+  document.getElementById('root')
+);
